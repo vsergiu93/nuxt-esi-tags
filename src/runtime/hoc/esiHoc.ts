81522c62b816e4c7a7d883bc1753072c218e0f71
Sergiu Daniel Vidican <vsergiu93@gmail.com>
2024-02-02 19:46:43 +0200

    refactor: Refactor the `esiHoc` function in `esiHoc.ts`
    The `esiHoc` function in the `esiHoc.ts` file has been refactored to improve readability and maintainability. Changes include:
    
    - Reorganizing imports to follow a consistent style.
    - Adding a type definition for `ExtraHeaders`.
    - Updating the function signature to use the `ExtraHeaders` type for the `extraHeaders` parameter.
    - Renaming the variable `extraH` to `extraHeaders` to improve clarity.
    - Updating the component name to include the wrapped component's name in the format: `<WrappedComponentName>-EsiHocComponent`.
    - Simplifying the setup function and removing the unnecessary variable `componentName`.
    - Using the `WrappedComponent.name` directly when passing it to the `esiIncludeComponent` component.
    
    These changes improve the code's maintainability and adhere to best practices for code organization and naming conventions.

--- src/runtime/hoc/esiHoc.ts
+++ src/runtime/hoc/esiHoc.ts
@@ -1,23 +1,25 @@
-import {defineComponent, h, resolveComponent} from 'vue';
-import type {DefineComponent, Component} from "vue";
+import { defineComponent, h, resolveComponent } from 'vue';
+import type { DefineComponent, Component } from 'vue';
 
-export function esiHoc<T extends Component>(WrappedComponent: T, extraHeaders: Record<string, string> = {}): DefineComponent {
-  const extraH = {
+interface ExtraHeaders {
+  [key: string]: string;
+}
+
+export function esiHoc<T extends Component>(WrappedComponent: T, extraHeaders: ExtraHeaders = {}): DefineComponent {
+  const extraH: ExtraHeaders = {
     'Cache-Control': 'max-age=120',
     ...extraHeaders
   }
 
   return defineComponent({
-    name: 'EsiHocComponent',
+    name: `${WrappedComponent.name}-EsiHocComponent`,
     inheritAttrs: false,
-    setup(props, {attrs}) {
+    setup(props, { attrs }) {
       return () => {
-        const componentName = WrappedComponent.name;
-
         const esiIncludeComponent = resolveComponent('EsiInclude');
 
         return h(esiIncludeComponent, {
-          componentName: componentName,
+          componentName: WrappedComponent.name,
           extraHeaders: extraH,
           ...attrs
         })
